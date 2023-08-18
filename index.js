238ccbf05a9b55a2d66ada60bc95effe11992b2f
James Hallberg <Grebllah81788@gmail.com>
2023-08-18 11:39:21 -0400

    final project cleanup

--- index.js
+++ index.js
@@ -34,7 +34,6 @@ let excludedItems = ["index", "url", "classes", "subclasses"]
 const extractObjInfo = (obj, parent) => {
     const infoObj = Object.entries(obj)
     infoObj.forEach(entry => {
-        // console.log(entry)
         if (!excludedItems.includes(entry[0])){
             if (typeof entry[1] === "object"){
                 const ul = document.createElement("ul")
@@ -47,7 +46,6 @@ const extractObjInfo = (obj, parent) => {
             }
         }
     })
-    //console.log(infoObj)
 }
 
 
@@ -58,31 +56,24 @@ const spellLookup = async (e) => {
     const spellName = e.srcElement.innerText.replace(/\s+/g, '-').toLowerCase();
     spellCard.appendChild(spellListHeader).innerText = e.srcElement.innerText
     spellURL = `https://www.dnd5eapi.co/api/spells/${spellName}`
-    //console.log(spellURL)
     const response = await fetch(spellURL)
     const data = await response.json()
-    //console.log(data)
     extractObjInfo(data, spellCard)
 }
 
 const filterSpell = async (spell) => {
-    //fetches the clicked on spell, and compares the level of the spell the level in the levelDropdown
     const spellURL = `https://www.dnd5eapi.co/api/spells/${spell}`
     const response = await fetch(spellURL)
     const data = await response.json()
-    // console.log(data)
     if (data.level <= parseInt(levels.value)){
-        //console.log("true")
         return true
     } else {
-        //console.log("false")
         return false
     }
 }
 
 const changeClass = async (e) => {
     e.preventDefault()
-    //when the submit button is clicked, the class information is fetched and extracted
     spellCard.innerHTML = ""
     if (classList.value !==""){
         classDescript.innerText = ""
@@ -91,14 +82,11 @@ const changeClass = async (e) => {
         classSpellList.innerHTML = ""
         equip.innerHTML = ""
         profList.innerHTML = ""
-        //clears previous displayed info
         const classes = classList.value.toLowerCase()
         const response = await fetch (`https://www.dnd5eapi.co/api/classes/${classes}`)
         const data = await response.json()
-        //console.log(data)
         const profs = data.proficiencies
         const equipment = data.starting_equipment
-        //extract the data, and create a section for proficiencies and spells and equipment
 
         const profsHeader = document.createElement("h3")
         classDescript.appendChild(profsHeader).innerText= `These are the ${classes}'s proficiencies`
@@ -110,38 +98,28 @@ const changeClass = async (e) => {
         const equipmentParagraph = document.createElement("h3")
         equip.appendChild(equipmentParagraph).innerText= `These are the ${classes}'s starting equipment`
         equipment.forEach(gear => {
-            // console.log(gear)
             const gearPiece = document.createElement("li")
             const name = gear.equipment.name
             equipList.appendChild(gearPiece).innerText= name
         })
         
         if (data.spells) {
-            //the spell list requires another fetch request
             const spellsHeader = document.createElement("h3")
             const spellResponse = await fetch (`https://www.dnd5eapi.co/api/classes/${classes}/spells`)
             const spellData = await spellResponse.json()
-            // console.log(spellData)
             const spells = spellData.results
-            //console.log(spells)
             classSpells.appendChild(spellsHeader).innerText= `These are the ${classes}'s available spells`
             spells.forEach(async spell => {
-                //console.log(spell)
                 const spellIndex = spell.index
                 const spellName = spell.name
-                // here is where I filter the results, as I cant access the spell list and the levels of each
-                // individual spell at the same time
                 const filtered = await filterSpell(spellIndex)
                 if (filtered){
-                    //console.log("true")
                     const spellTitle = document.createElement("li")
                     classSpellList.appendChild(spellTitle).innerText= spellName
                     spellTitle.className= "spell"
                     spellTitle.addEventListener("click", spellLookup)
                 }
             })
-
-
         }
     }
 }
